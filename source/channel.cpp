913272d94a15e0b52a92e7d193e651308e36558e
0bo084 <eighthur@gmail.com>
2022-10-10 12:01:36 +0300

    add comments

--- source/channel.cpp
+++ source/channel.cpp
@@ -12,17 +12,17 @@ void deviceChannel::set(dataProvider& prov)
     // use exclusive lock while writing data
     std::unique_lock<std::shared_mutex> lock(mtx);
     
-
+    // set probabiliti of causing busy state (5%)    
     static constexpr double probability = 0.05;
     //divide to get a number between 0 and 1
     double result = rand() / static_cast<double>(RAND_MAX);
    
     if(getStatus() == status_t::busy)
-        setStatus(prevstatus);
+        setStatus(prevstatus); // restore prev status before busy
     else if((result < probability)&&(getStatus() != status_t::busy)){
-        // emulate probabitity error
-        prevstatus = getStatus();
-        setStatus(status_t::busy);
+        // emulate probabitity error,  
+        prevstatus = getStatus(); // store current status
+        setStatus(status_t::busy); // set busy 
     }
     
     if (getStatus() == status_t::measure) {
